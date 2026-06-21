be973823023742f157719a35a9489de805590c4e
lizkueez <liz@kueez.com>
2026-06-21 17:34:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -173,10 +173,17 @@ if generate:
             articles_report
         )
 
-        st.write("Columns found:")
-        st.write(
-            list(articles_df.columns)
-        )
+title_column = "Original Article Name"
+
+top_titles = (
+    articles_df[title_column]
+    .astype(str)
+    .head(30)
+    .tolist()
+)
+
+st.write("Top Titles:")
+st.write(top_titles[:5])
 
     # ---------------- GEO ----------------
 
